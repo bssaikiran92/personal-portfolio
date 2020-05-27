7d03fe328b9ce70e0ae93274ca4479180c1518af
saibojan <bssaikiran92@gmail.com>
2020-05-27 00:19:16 -0700

    first commit

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+
+def all_blogs(request):
+    blog_count = Post.objects.count
+    posts = Post.objects.order_by('-created_date')[:2]
+    return render(request, 'blog/all_blogs.html', {'posts': posts, 'blog_count': blog_count})
+
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Post, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog': blog})
